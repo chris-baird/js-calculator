72e2d0a2141345cbc420db664cae39d103bcafbf
chris baird <cab3953@gmail.com>
2016-12-02 18:44:43 -0800

    added logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 /*--- variables ---*/
 
-var input, firstNum, Op, result;
-var displayEl = document.getElementById('displayEl');
-
+var input, firstNum, op, result;
+var displayEl = document.getElementById('display');
 
 /*--- event listeners ---*/
+document.getElementById('calc')
+  .addEventListener('click', handleClick);
 
 
 
@@ -13,16 +14,89 @@ var displayEl = document.getElementById('displayEl');
 
 /*--- functions ---*/
 
-function initialize() {
-  input = '';
-  firstNum = oP = result = null;
+function initialize () {
+ input = '';
+ firstNum = op = result = null;
+ updateDisplay();
+}
+
+function updateDisplay(){
+ var text = result || input || '0'
+ text += input.includes('.') ? '' : '.';
+ displayEl.textContent = text;
+ if (result) {
+ input = '';
+ op = firstNum = result = null;
+}
+}
+
+
+
+function handleClick(evt) {
+  var clickedEl = evt.target
+  if (clickedEl.id === 'display') return;
+  switch (clickedEl.textContent) {
+    case 'CA':
+      initialize();
+      break;
+    case '+' :
+      assignOp(add);
+      break;
+    case '←' :
+      input = input.substr(0, input.length - 1);
+      break;
+    case '-' :
+      assignOp(sub);
+      break;
+    case '÷' :
+      assignOp(div);
+      break;
+    case '=' :
+      if (!op || !input) return;
+      result = op(parseFloat(firstNum), parseFloat(input));
+      break;
+    case '×' :
+      assignOp(mult);
+      break;
+    case '.' :
+      input += input.includes('.') ? '' : '.';
+      break;
+    default:
+      input += clickedEl.textContent;
+  }
   updateDisplay();
 }
 
-function updateDisplay() {
-  var text = result || input || '0.';
-  displayEl.textContent = text;
-
+function assignOp(clickedOp) {
+  if (!input) return;
+  op = clickedOp;
+  firstNum = input;
+  input = '';
 }
 
+
+
+
+
+
 initialize();
+
+/*-- operator */
+
+function add(x, y) {
+  return x + y;
+}
+
+function sub(x, y) {
+  return x - y;
+}
+
+function mult(x, y) {
+  return x * y;
+}
+
+function div(x, y) {
+  return x / y;
+}
+
+
